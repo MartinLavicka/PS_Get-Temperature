fc2da342c84abd2b03a6dff63023563c3a176498
MartinLavicka <MartinLavicka@users.noreply.github.com>
2016-12-06 14:58:12 +0100

    Update get-temperature.psm1

--- get-temperature.psm1
+++ get-temperature.psm1
@@ -1,17 +1,12 @@
 function Get-Temperature {
-    $t = Get-WmiObject MSAcpi_ThermalZoneTemperature -Namespace "root/wmi"
+    $t = @( Get-WmiObject MSAcpi_ThermalZoneTemperature -Namespace "root/wmi" )
     $returntemp = @()
-
-    foreach ($temp in $t.CurrentTemperature)
+    foreach ($temp in $t)
     {
-
-
-    $currentTempKelvin = $temp / 10
-    $currentTempCelsius = $currentTempKelvin - 273.15
-
-    $currentTempFahrenheit = (9/5) * $currentTempCelsius + 32
-
-    $returntemp += $currentTempCelsius.ToString() + " C : " + $currentTempFahrenheit.ToString() + " F : " + $currentTempKelvin + "K"  
+        $currentTempKelvin = $temp.CurrentTemperature / 10
+        $currentTempCelsius = $currentTempKelvin - 273.15
+        $currentTempFahrenheit = (9/5) * $currentTempCelsius + 32
+        $returntemp += $currentTempCelsius.ToString() + " C : " + $currentTempFahrenheit.ToString() + " F : " + $currentTempKelvin + "K"  
     }
     return $returntemp
 }
